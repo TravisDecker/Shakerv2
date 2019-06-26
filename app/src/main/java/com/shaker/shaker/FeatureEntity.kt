89f6c6bb0cf9ba666ab843293220079f2f2db8c1
Travis Decker <decker4693@gmail.com>
2019-06-25 21:35:19 -0600

    Updated build files, added plugin, created Feature Entity

---
+++ app/src/main/java/com/shaker/shaker/FeatureEntity.kt
@@ -0,0 +1,14 @@
+package com.shaker.shaker
+
+import androidx.annotation.NonNull
+import androidx.room.PrimaryKey
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class FeatureEntity(
+    @PrimaryKey @NonNull val featureId: Int, val featureType: String,
+    val properties: PropertiesEntity,
+    val Geometry: GeometryEntity
+) {
+
+}
