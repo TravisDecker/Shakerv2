47c6a3f641fd52c0c7c73c60400e2d16f84a301d
Travis Decker <decker4693@gmail.com>
2019-06-27 18:20:02 -0600

    created feature class, completed geometry class

--- app/src/main/java/com/shaker/shaker/FeatureEntity.kt
+++ app/src/main/java/com/shaker/shaker/FeatureEntity.kt
@@ -6,7 +6,8 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class FeatureEntity(
-    @PrimaryKey @NonNull val featureId: Int, val featureType: String,
+    @PrimaryKey @NonNull val featureId: Int,
+    val featureType: String,
     val properties: PropertiesEntity,
     val Geometry: GeometryEntity
 ) {
--- app/src/main/java/com/shaker/shaker/GeometryEntity.kt
+++ app/src/main/java/com/shaker/shaker/GeometryEntity.kt
@@ -4,8 +4,18 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class GeometryEntity(
-    val mag: Double, val palce: String, val time: Long, val updated: Long, val url: String, val felt: Long,
-    val alert: String, val status: String, val net: String, val ids: String, val nst: Long, val title: String
+    val mag: Double,
+    val palce: String,
+    val time: Long,
+    val updated: Long,
+    val url: String,
+    val felt: Long,
+    val alert: String,
+    val status: String,
+    val net: String,
+    val ids: String,
+    val nst: Long,
+    val title: String
 ) {
 
 }
